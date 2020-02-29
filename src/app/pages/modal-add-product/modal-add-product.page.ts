208d59ec69edf8fe3b3ebe40825d3895aa3fd23f
Eduardo Vergara <eduver_san@hotmail.com>
2020-02-28 20:01:00 -0500

    added view price in addProduct

--- src/app/pages/modal-add-product/modal-add-product.page.ts
+++ src/app/pages/modal-add-product/modal-add-product.page.ts
@@ -6,6 +6,8 @@ import { ModalComparePage } from '../modal-compare/modal-compare.page';
 import { StorageService } from '../../services/storage.service';
 import { Storage } from '@ionic/storage';
 import { ModalProductPage } from '../modal-product/modal-product.page';
+import { BuyService } from '../../services/buy.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-modal-add-product',
@@ -15,6 +17,7 @@ import { ModalProductPage } from '../modal-product/modal-product.page';
 export class ModalAddProductPage implements OnInit {
 
   products = [];
+  newProducts = [];
   categories = [];
   textToSearch = '';
 
@@ -24,17 +27,46 @@ export class ModalAddProductPage implements OnInit {
     private alertCtrl: AlertController,
     private uiService: UiServiceService,
     private storageService: StorageService,
-    private storage: Storage
+    private storage: Storage,
+    private buyService: BuyService
   ) { }
 
-  ngOnInit() {
-    this.marketsService.getProducts().subscribe(resp => {
+  async ngOnInit() {
+    await this.marketsService.getProducts().subscribe(resp => {
       this.products = [...resp];
+      // console.log(this.products);
+      this.getBuys();
     });
 
     this.marketsService.getCategories().subscribe(resp => {
       this.categories.push(...resp.categories);
     });
+  }
+
+  getBuys() {
+    this.products.forEach(x => {
+      // console.log(x);
+      this.buyService.getItem(x._id).subscribe(resp => {
+        // console.log(resp);
+        // tslint:disable-next-line: no-string-literal
+        if (resp['count'] === 0) {
+          this.newProducts.push(x);
+        } else {
+          // tslint:disable-next-line: no-string-literal
+          const buys = [...resp['buys']].forEach((value, index) => {
+            const item = value.items.filter(y => {
+              // console.log(y); // y es un objeto tipo item
+              if (y._id === x._id) {
+                return true;
+              }
+            });
+            console.log(item);
+            this.newProducts.push(...item);
+          });
+        }
+      });
+    });
+    console.log(this.newProducts);
   }
 
   dismissModal() {
@@ -48,8 +80,7 @@ export class ModalAddProductPage implements OnInit {
   async selectedProduct(item) {
     const alert = await this.alertCtrl.create({
       header: item.product,
-      subHeader: item.subcategory.subcategory,
-      message: 'This is an alert message.',
+      subHeader: JSON.stringify(item),
       inputs: [
         {
           name: 'gramaje',
